7c6addd0dcbc32111c10d1749931d310d7dbae99
Isaac <isaac.yeo.linux@gmail.com>
2019-10-24 18:30:30 +0800

    Restored my sanity

--- fileio/headers/bufman.h
+++
@@ -1,7 +0,0 @@
-#ifndef INCLUDE_INCLUDE
-#include "../../include.h"
-#endif
-#define INCLUDE_BUFMAN
-
-bool add_buf(editor_status *estat, char *filename);
-void close_buf(editor_status *estat, char *filename);
